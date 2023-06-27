08d3ba5587b1f1c7465759571752c2d660841191
liuqin <1263898254@qq.com>
2023-06-27 09:54:44 +0800

    组合式API

--- Vue/03_vue的基础/src/App.vue
+++ Vue/03_vue的基础/src/App.vue
@@ -1,81 +1,43 @@
-<script>
+<script setup>
+import { reactive } from 'vue';
+import { ref } from 'vue';
+import {$ref} from "vue/macros"
 
-export default {
-  //data返回的对象最终会被vue代理，写在函数中是为了每一次调用都返回一个新的对象，互不干扰
-  data() {
-    return {
-      msg: "今天天气不错！",
-      stu:{
-        name:"孙悟空",
-        age:18,
-        gender:"男"
-      },
-      lastName:"边",
-      firstName:"浩东",
-      arr:["猪八戒", "孙悟空", "沙和尚"],
-    }
-  },
+/* 
+  reactive()
+    返回一个对象的响应代理(深层响应代理)
+    也可以使用shallowReactive()创建一个浅层响应式对象
+    缺点：只能返回对象的响应式代理，不能处理原始值
 
-  methods:{
-    sum(a, b){
-      //console.log(this);//组件实例vm
-      return a + b;
-    },
-    changeMsg(){
-      this.msg = "新的消息！"
-    },
-    updateAge(){
-      if(this.stu.age === 18){
-        this.stu.age = 17;
-      }else{
-        this.stu.age = 18;
-      }
-    },
-    getInfo(){
-      console.log("getInfo被调用了");
-      return this.stu.age >= 18 ? "你是一个成年人!!" : "你是一个未成年人!!"
-    }
-  },
-  /* 
-    computed用来指定计算属性
-    {
-      属性名：getter
-    }
-    - 计算属性，只在其依赖的数据发生变化时才会重新执行
-    - 会对数据进行缓存
-  */
-  computed:{
-    info(){
-      console.log("info被调用了");
-      return this.stu.age >= 18 ? "你是一个成年人" : "你是一个未成年人"
-    },
-    //计算属性的简写（只有getter时）
-    /* name(){
-      return this.lastName + this.firstName;
-    }, */
-    name:{
-      get(){
-        return this.lastName + this.firstName;
-      },
-      set(value){
-        //set在计算属性被修改时调用
-        //console.log(value);
-        this.lastName = value[0];
-        this.firstName = value.slice(1);
-      }
-    }
+  ref()
+    接收一个任意值，并返回它的响应式代理
+*/
+  const stu = reactive({
+    name:"孙悟空",
+    age:18,
+    gender:"男"
+  });
+  //console.log(stu);
+
+  //ref在生成响应式代理时，它是将值包装为了一个对象，0 --> {value:0}
+  //访问ref对象时，必须通过 对象.value 来访问其中的值
+  //在模板中，ref对象会被自动解包
+  //let count = ref(0);//生成一个0的响应式代理
+  //console.log(count.value);
+
+  //vue提供了一个语法糖，使得ref对象在script标签中也可以自动解包
+  //$是一个实验性的，需要在vite插件中做一些配置 reactivityTransform:true
+  let count = $ref(1);
+  console.log(count);
+
+  //count = 10; //改变量只会影响到变量自己，在js中，无法实现对一个变量的代理
+  function fn() {
+    count++;
   }
-}
 </script>
 
 <template>
-  <h2>{{ msg }}</h2>
-  <h2>{{ sum(2, 5) }}</h2>
-  <button @click="changeMsg">点我一下</button><!-- 这里执行修改msg时，methods中的getInfo()会被调用，而computed中的info()不会被调用 -->
-  <h2>{{ stu.name }} --- {{ stu.age }} --- {{ stu.gender }}</h2>
-  <h2>computed:{{ info }}</h2>
-  <h2>methods:{{ getInfo() }}</h2>
-  <button @click="updateAge">变年龄</button>
-  <h1>{{ name }}</h1>
-  <h1>{{ arr[0] }} --- {{ arr[1] }} --- {{ arr[2] }}</h1>
+  <h1>组合式API</h1>
+  <h2>{{ stu.name }}---{{ stu.age }}---{{ stu.gender }}</h2>
+  <h2 @click="fn">{{ count }}</h2>
 </template>
