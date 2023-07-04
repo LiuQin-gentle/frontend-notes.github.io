107bd02f66bc82457db5b96fecc09eab46f82944
liuqin <1263898254@qq.com>
2023-07-04 14:28:17 +0800

    模板语法

---
+++ Vue/04_模板/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref } from 'vue';
+  const imgPath = ref("/images/messi.png");
+  const changeImg = () => {
+    imgPath.value = "/images/neymar.png"
+  }
+  const attrs = {
+    id: "box1",
+    class: "hello"
+  }
+</script>
+
+<template>
+  <!-- 
+    当需要为标签动态设置属性时，需要使用v-bind指令，
+      v-bind可以简写为 :
+   -->
+  <img v-bind:src="imgPath" alt="梅西"/>
+  <button @click="changeImg">切换图片</button>
+  <!-- <div v-bind="attrs"></div> -->
+  <div :="attrs"></div>
+</template>
